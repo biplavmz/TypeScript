f35ca44396855cee7e6ea2aa4ebbf5d90a460391
Biplav <biplav.mazumdar@terralogic.com>
2023-12-12 14:35:58 +0530

    Storage In Browser

---
+++ src/Theory.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Theory = () => {
+  return (
+    <>
+    <div>Redux having Store </div>
+    <p>where store is the single source of truth or we can say that central store for storing the data</p>
+
+    <h2>Then we have reducer </h2>
+    <h1>all the functionality part or changes  will happens with the help of Reducers </h1>
+    <p>Reducres are nothing they are the objects </p>
+
+    <h1>Then we have useSelector and useDispatch</h1>
+    <p>Selector is when we have to select something </p>
+
+    <p>Dispatch when we have to Dispatch any thing </p>
+
+    <h1>For the Installation we are using npm install @reduxjs/toolkit react-redux</h1>
+
+    <h2>For creating the Store create a Folder Name called App under that give the name Store.js</h2>
+
+
+    <h1>Create the Store </h1>
+
+    <p>To create the store we have to</p>
+    <ul>
+        <li>Step1 :- To create the Store we have to First Configure the store</li>
+        <li></li>
+    </ul>
+    
+    </>
+  )
+}
+
+export default Theory
